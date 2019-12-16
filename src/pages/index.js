40186a0174e541ceaedaf1705a3b76a195d308fd
lichengzhi <lichengzhi@cnicg.cn>
2019-12-16 13:06:58 +0800

    add tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,14 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+      <div className="ml-6 pt-1">
+        <h4 className="text-xl text-gray-900 leading-tight">ChitChat</h4>
+        <p className="text-base text-gray-600 leading-normal">
+          You have a new message!
+        </p>
+      </div>
+    </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
